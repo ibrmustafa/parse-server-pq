0aa68cf6a82114b2feb3d06379140be478353c06
Mahesh <mmkdissa@gmail.com>
2018-05-14 01:25:49 +0530

    revert hardcoded changes and backed to enviornment varibales

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var express = require('express');
 var ParseServer = require('parse-server').ParseServer;
 var path = require('path');
 
-var databaseUri = 'mongodb://heroku_cknf11mk:jsu11t38acphq6v2s6ht8baln7@ds147070.mlab.com:47070/heroku_cknf11mk';//process.env.DATABASE_URI || process.env.MONGODB_URI;
+var databaseUri = process.env.DATABASE_URI || process.env.MONGODB_URI;
 
 if (!databaseUri) {
 	console.log('DATABASE_URI not specified, falling back to localhost.');
@@ -12,22 +12,22 @@ if (!databaseUri) {
 
 var api = new ParseServer({
 
-    databaseURI: 'mongodb://heroku_cknf11mk:jsu11t38acphq6v2s6ht8baln7@ds147070.mlab.com:47070/heroku_cknf11mk',//process.env.MONGODB_URI,
+    databaseURI: process.env.MONGODB_URI,
     cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
-    serverURL: process.env.SERVER_URL || 'https://peppeq-mahesh.herokuapp.com/parse',
+    serverURL: process.env.SERVER_URL,
     allowClientClassCreation: process.env.CLIENT_CLASS_CREATION || true, 
-    appId: process.env.APP_ID || 'mySampleAppID',
-    masterKey: process.env.MASTER_KEY || 'myMasterKey',
-    publicServerURL: process.env.SERVER_URL || 'https://peppeq-mahesh.herokuapp.com/parse',
+    appId: process.env.APP_ID,
+    masterKey: process.env.MASTER_KEY,
+    publicServerURL: process.env.SERVER_URL,
     verifyUserEmails: true,
     appName: "peppequeue",
     
     emailAdapter: {
 	module: 'parse-server-mailgun',
 	options: {
-	    fromAddress: process.env.EMAIL_FROM || 'PeppeQ<noreply@yourapp.com>' ,
-	    domain: process.env.MAILGUN_DOMAIN || 'sandbox7f4515dafa424dad80e674d3c130df6c.mailgun.org',
-	    apiKey: process.env.MAILGUN_API_KEY ||'key-eef7acd0aab4f788efde54e5e925305d' ,
+	    fromAddress: process.env.EMAIL_FROM,
+	    domain: process.env.MAILGUN_DOMAIN,
+	    apiKey: process.env.MAILGUN_API_KEY,
 
 	    templates: {
 		passwordResetEmail: {
@@ -67,10 +67,11 @@ app.get('/', function (req, res) {
 	res.sendFile(path.join(__dirname, '/public/login.html'));
 });
 
-
 app.get('/dashboard', function(req, res) {  
    res.sendFile(path.join(__dirname, '/public/dashboard.html'));
 });
+
+
 
 var port = process.env.PORT || 1337;
 var httpServer = require('http').createServer(app);
